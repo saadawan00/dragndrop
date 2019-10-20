0e681ef3d910d468e1844ed725e60ae781e08e5e
Saad Awan <msaadawan0@gmail.com>
2019-10-20 18:32:54 +0500

    div drop zone

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    files: []
+  }
+    inputRef = React.createRef();
+    onDrag = (e) => {
+      e.preventDefault(); 
+    // console.log("e allowdrop", e)
+
+    }
+
+    onDrop = (e) => {
+      e.preventDefault(); 
+      e.stopPropagation();
+      // console.log('ref', this.inputRef)
+    console.log("e drop data", e.dataTransfer)
+    console.log("e drop", e.target)
+    }
+
+    handleChange = (e) => {
+      console.log("files", e.target.files)
+    }
+
+    render(){
+      return (
+        <div className="App">
+        <div  className="App-header"  onDrop={this.onDrop} onDragOver={this.onDrag}  >
+          div 1
+          <input type="hidden" id="input" style={{width: "100%", height: "100%"}} accept=".png,.jpg" onChange={this.handleChange} multiple aria-label="upload images" />
+
+        </div>  
+
+        <div id="2" className="App-header">
+          div 2
+        </div>
+        </div>
+      );
+    }
 }
 
 export default App;
